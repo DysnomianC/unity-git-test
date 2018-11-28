3ba368ead18cfabb8bf7c06c0e8404aaab11ba9c
NeptunianEclipse <neptunianeclipse@gmail.com>
2018-11-29 00:45:02 +1300

    Added DoStuff.cs

---
+++ Assets/DoStuff.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoStuff : MonoBehaviour {
+
+	void Start () {
+        Debug.Log("Doing stuff yup totally");
+	}
+	
+	
+}
